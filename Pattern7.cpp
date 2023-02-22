085cbc28063b325da3ae1d25ef7853ebe155ee75
Manoj Joshi <108046894+Mj1781@users.noreply.github.com>
2023-02-22 20:58:50 +0530

    Add files via upload

---
+++ Pattern7.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int row=1;
+	while(row<=n){
+		int col=1;
+			cout<<row;
+			col=col+1;
+		}
+		cout<<endl;
+		row=row+1;
+	}
+}
